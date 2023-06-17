165d13a3731f7278ad07762ed09fc4987376d251
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-06-17 11:07:06 +0300

    add required fields

--- src/articles/Article.schema.ts
+++ src/articles/Article.schema.ts
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
-
-
-
 export const articleSchema = new mongoose.Schema({
+  slug: {
+    type: String,
+    require: true
+  },
   title: {
     type: String,
     required: true,
     minlength: 6,
   },
   text: String,
-  description: String
+  description: String,
+  dateCreate: {
+    type: Date,
+    default: Date.now
+  }
 });
