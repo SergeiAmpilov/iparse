6b51c83bac070fbe91c03c2447b09ae5f899242e
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-06-27 11:11:22 +0300

    feat: move static

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,29 @@
 import bodyParser from 'body-parser';
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 import { ArticleDto } from './articles/Article.dto';
 import { ArticleModel } from './articles/Article.model';
 
+dotenv.config();
+const { PORT = 3002}  = process.env;
+const { DB_NAME = 'iparsebd'}  = process.env;
+
+
 
 const app: Express = express();
-const port = 3000;
 
 app.use(bodyParser.json());
+app.use(
+  express.static(path.join(path.dirname(__dirname), 'public' ))
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Main page');
 });
 
+/*
 app.get('/services/:slug', (req: Request, res: Response) => {
   res.send(`Service detail page with slug ${req.params?.slug}`);
 });
@@ -43,15 +53,15 @@ app.post('/articles', async ({ body }: Request<{}, {}, ArticleDto>, res: Respons
     ok: result
   })
 });
-
+*/
 
 
 
 async function start() {
   await mongoose.connect('mongodb://localhost:27017/parsedb');
 
-  app.listen(port, () => {
-    console.log(`Server has been started at http://localhost:${port}/!`);
+  app.listen(PORT, () => {
+    console.log(`Server has been started at http://localhost:${PORT}/!`);
   })
 }
 
